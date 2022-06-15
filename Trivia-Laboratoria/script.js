b0a7d0f4fd1f95876848ce902d53c3c9ff9a729e
ivetita713 <england_713@hotmail.com>
2022-06-14 21:23:02 -0500

    Version 0.3 - Se mejoro la interfaz visual y la validacion

--- Trivia-Laboratoria/script.js
+++ Trivia-Laboratoria/script.js
@@ -18,25 +18,15 @@ function answer1(data){ // inicio de la funcion y se asigna el valor
    console.log(av1);
  }
  else if(data == 'q1a3'){ 
-   console.log('q1a3'); 
    document.getElementById('q1a3').checked=true;
    document.getElementById('q1a1').checked=false;
    document.getElementById('q1a2').checked=false;
+   av1=document.getElementById('q1a3').value;
+   console.log(av1); 
  }
  else { console.log('error'); }
 };
-function answer2(data){
- console.log(data);
-};
-function validate(){
-  if(av1 == 'Tom Cruise'){
-    document.getElementById('q1p1').style.color="green"
-  } else {
-    document.getElementById('q1p1').style.color="red"
-  }
-};
-// aquí inicia la pregunta dos
-
+//aquí inicia la pregunta dos
 function answer2(data){ // inicio de la funcion y se asigna el valor
  if(data == 'q2a1'){    //evaluar el valor de la variable 
    //alert('q1a1');       // indica cuál es la opción seleccionada 
@@ -55,7 +45,6 @@ function answer2(data){ // inicio de la funcion y se asigna el valor
    console.log(av2);
  }
  else if(data == 'q2a3'){ 
-   console.log('q2a3'); 
    document.getElementById('q2a3').checked=true;
    document.getElementById('q2a1').checked=false;
    document.getElementById('q2a2').checked=false;
@@ -64,13 +53,22 @@ function answer2(data){ // inicio de la funcion y se asigna el valor
  }
  else { console.log('error'); }
 };
-function answer2(data){
- console.log(data);
-};
+
 function validate(){
-  if(av2 == 'Emma Watson'){
-    document.getElementById('q2p1').style.color="green"
-  } else {
-    document.getElementById('q2p1').style.color="red"
+  if(av1 == 'Tom Cruise'){ //correcto
+    document.getElementById('q1p1').style.color="#55c624"
+    if(av2 == 'Emma Watson'){ //correcto
+      document.getElementById('q2p1').style.color="#55c624"
+      alert('¡Bien hecho!');
+    } else { //incorrecto
+      document.getElementById('q2p1').style.color="red"
+    }
+  } else { //incorrecto
+    document.getElementById('q1p1').style.color="red"
+    if(av2 == 'Emma Watson'){ //correcto
+      document.getElementById('q2p1').style.color="#55c624"
+    } else { //incorrecto
+      document.getElementById('q2p1').style.color="red"
+    }
   }
 };
